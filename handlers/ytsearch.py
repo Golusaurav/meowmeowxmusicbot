91239c69dc5ae78da649e3dc5322c2989a070063
Keeplaughingxusertagbot <107563739+Keeplaughingxusertagbot@users.noreply.github.com>
2022-07-06 21:10:41 +0530

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -31,10 +31,10 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 4:
-            text += f"🥀ᴛɪᴛʟᴇ - {results[i]['title']}\n"
-            text += f"👩‍💻ᴅᴜʀᴀᴛɪᴏɴ - {results[i]['duration']}\n"
-            text += f"💬ᴠɪᴇᴡs - {results[i]['views']}\n"
-            text += f"💥ᴄʜᴀɴɴᴇʟ - {results[i]['channel']}\n"
+            text += f"😻ᴛɪᴛʟᴇ😻 - {results[i]['title']}\n"
+            text += f"😸ᴅᴜʀᴀᴛɪᴏɴ😸 - {results[i]['duration']}\n"
+            text += f"😼ᴠɪᴇᴡs😼 - {results[i]['views']}\n"
+            text += f"😽ᴄʜᴀɴɴᴇʟ😽 - {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
